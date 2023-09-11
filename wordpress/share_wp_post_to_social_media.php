df693cc4cbb99926221c2450a6c363732f1ec463
Shahadat Hossain <44918480+palash-wp-dev@users.noreply.github.com>
2023-09-11 15:23:53 +0600

    Update share_wp_post_to_social_media.php

--- wordpress/share_wp_post_to_social_media.php
+++ wordpress/share_wp_post_to_social_media.php
@@ -1,1 +1,8 @@
+<?php
+$current_page_link = get_permalink( get_the_ID() ); // get current post page link
 
+$csf_facebook_share_url = 'https://www.facebook.com/sharer/sharer.php?u=' . $current_page_link;
+$csf_twitter_share_url = 'https://twitter.com/intent/tweet?text=' . $current_page_link;
+$csf_linkedin_share_url = 'https://www.linkedin.com/shareArticle?mini=true&url=' . $current_page_link;
+$csf_youtube_share_url = 'https://www.youtube.com/watch?v=' . $current_page_link;
+$csf_pinterest_share_url = 'https://pinterest.com/pin/create/button/?url=' . $current_page_link;
