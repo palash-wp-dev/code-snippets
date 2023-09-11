3cf4d7fae886cf4efa7254e0204308073d1dc62d
Shahadat Hossain <44918480+palash-wp-dev@users.noreply.github.com>
2023-09-11 13:14:08 +0600

    Update get_tag_archive_link.php

--- wordpress/get_tag_archive_link.php
+++ wordpress/get_tag_archive_link.php
@@ -1,7 +1,7 @@
 <?php
 // This will create a tag link which will take the user to that particular tag archive page where they will see all posts related to only that tag
 $tag = get_the_tags();
-$tag_slug = $tag[0]->slug; // here we will only get the slug of the first tag, if we want all the tags then we will have to run a WP loop
+$tag_slug = $tag[0]->slug; // here we will only get the slug of the first tag, if we want all the tags then we will have to run a 'foreach' loop
 $tag_link = get_tag_link( get_term_by('slug', $tag_slug, 'post_tag') );
 ?>
 <a href="<?php echo esc_url( $tag_link ); ?>" class="blog__item__contents__tag__item">
