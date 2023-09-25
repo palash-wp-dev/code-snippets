bc6c417ccba9525eef3c89b5706eea502daa137f
palash-wp-dev <raps.palash.840@gmail.com>
2023-09-26 00:39:56 +0600

    added a folder called wp-cli

---
+++ wp-cli/wp-cli.php
@@ -0,0 +1,6 @@
+<?php 
+// check whether wp-cli is installed or not
+wp --info
+
+//create .pot file for plugin or theme in the languages folder
+wp i18n make-pot . languages/hexcoupon.pot // replace hexcoupon.pot with your own plugin file name. like myplugin.pot
