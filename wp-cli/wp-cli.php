257e407e32df955ff3008f2825ecf81d4c2b7b19
palash-wp-dev <raps.palash.840@gmail.com>
2024-08-21 23:54:53 +0600

    Add some git command

--- wp-cli/wp-cli.php
+++ wp-cli/wp-cli.php
@@ -5,7 +5,7 @@ wp --info
 //create .pot file for plugin or theme in the languages folder
 wp i18n make-pot . languages/hexcoupon.pot // replace hexcoupon.pot with your own plugin file name. like myplugin.pot
 
-poco x6
+
 realme 12+ 5g
 samsung galaxy a34
 poco f4
@@ -13,3 +13,26 @@ poco f4
 
 realme narzo 70 pro
 redmi turbo 3
+vivo v30 lite 4g
+
+liked phones
+============
+Xiaomi Poco M6 Pro
+Xiaomi Poco X6
+Xiaomi Redmi K70 Ultra
+Xiaomi Redmi K70e
+
+Xiaomi Redmi Turbo 3 // most liked
+redmi note 13 pro plus // most liked
+
+Infinix Note 30 Pro
+Tecno Camon 20 Pro
+
+Samsung Galaxy A24 and a25
+realme narzo 60 5g
+realme 11 x
+realme p1
+moto g84 and g64
+iqoo z9
+redmi note 13
+poco x6 neo
