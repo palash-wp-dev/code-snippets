87336e67dca151cab56fb9505bfadcd82b09629a
palash-wp-dev <raps.palash.840@gmail.com>
2025-01-19 19:00:58 +0600

    Added some commnent in the composer folder

--- php/composer/all_commands.php
+++ php/composer/all_commands.php
@@ -1,12 +1,16 @@
 <?php
+composer // to check whether composer is installed globally or not
+
 composer --version // to check the installed version of composer
 
 composer init // it will initialize composer and will add composer.json file in the project
 
-composer install // it will install composer.lock file if there are no composer.lock does not exist
+composer install // it will download and install packages based on composer.json (and composer.lock if present) file requirements.
 
-composer require // it will install whatever dependency you want to add
+composer require // it will install whatever dependency you want to add (for both development and deployment)
 e.g. composer require psr/log
+
+composer require --dev // it will install whatever dependency you want to add (for both development)
 
 composer update // it will update all outdated things
 
@@ -17,6 +21,8 @@ composer outdate // it will show all outdated dependencies of the project
 composer remove // it will remove dependency from the composer
 e.g. composer remove psr/log
 
-composer dump-autoload // it will 
+composer dump-autoload // ensures that Composer’s autoloader is up-to-date with your project structure.
 
 composer install --no-dev --optimize-autoloader // it will automatically add vendor folder with necessary dependancy 
+
+require('vendor/autoload.php'); // to use any package via composer in any php file, you need to require this file into the file
