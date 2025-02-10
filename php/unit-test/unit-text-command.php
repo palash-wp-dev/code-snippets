73022bd6228199c8856578a7758654d175a8b75a
palash-wp-dev <raps.palash.840@gmail.com>
2025-02-10 22:44:00 +0600

    Added some commands in the unit-test folder

--- php/unit-test/unit-text-command.php
+++ php/unit-test/unit-text-command.php
@@ -8,15 +8,23 @@
 2. Each method test one thing only
 3. Only one assert in one method
 4. Unit test has to be decoupled
-5. Test doesn't interfere each other
-6. Don't share data with tests
+5. Test does not interfere each other
+6. Don not share data with tests
 7. Ensure that each part of program is working fine as an isolate system
 
 
 /** All commands */
-to install phpunit you should check the compatible version of phpunit based on your php version
+// to install phpunit you should check the compatible version of phpunit based on your php version
 composer require --dev phpunit/phpunit ^12 //use this to install phpunit in your project as devlopment dependencies
+
 ./vendor/bin/phpunit --version or vendor/bin/phpunit --version //use this to check intall phpunit version
+
 php vendor/bin/phpunit --version //use this command if the above command is not working, that's because windows sometimes does not allow the above command
 
 php vendor/bin/phpunit tests // use this command to check phpunit test is working or not, but before you have to create folder named 'tests'
+
+// use better phpunit extension for testing purpose of phpunit in vscode
+$this->assertTrue();
+$this->assertEquals();
+$this->assertSee();
+$this->assertSeeInOrder();
