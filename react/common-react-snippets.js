25bf882e843ef3c5cbf8affeb96f22716883b27f
palash-wp-dev <raps.palash.840@gmail.com>
2025-06-02 00:16:44 +0600

    Add most common react code snippets for code autocompletion

---
+++ react/common-react-snippets.js
@@ -0,0 +1,16 @@
+// Most Common React Snippets
+// Shortcut	Expands To
+rafc	React Arrow Function Component with export
+rafce	React Arrow Function Component with default export
+rafcp	React Arrow Function Component with Props
+rafcpe	React Arrow Function Component with Props + default export
+rfc	React Function Component (normal function syntax)
+rfce	React Function Component with default export
+rcc	React Class Component
+rcce	React Class Component with default export
+rconst	Constructor inside a class component
+rsc	React Stateless Component (old pattern)
+rsf	React Stateless Function
+redux	Basic Redux setup
+useState	React useState hook snippet
+useEffect	React useEffect hook snippet
